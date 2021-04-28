811e97581c39a0849519b223ce4794099c05395f
Şaban Kayış <sbnkys1@gmail.com>
2021-04-28 08:46:32 +0300

    arrow functions

--- play.js
+++ play.js
@@ -1,11 +1,17 @@
 let namee = "Max";
 var age = 29;
- hasHobbies = true;
+hasHobbies = true;
 
 namee=" Şaban ";
 
-//const is never change.
+//const is never change, but let is can changed.
 // hasHobbies = false;
+
+//arrow functions
+const add = (a,b) => a + b;
+const addOne = a => a + 1;
+const addRandom = () => 1 + 2;
+
 function summarizeUser( userName, userAge, userHasHobby){
     return(
         "Name is"+
@@ -18,3 +24,4 @@ function summarizeUser( userName, userAge, userHasHobby){
 };
 
 console.log(summarizeUser(namee,age,hasHobbies));
+console.log(add(1,2));
