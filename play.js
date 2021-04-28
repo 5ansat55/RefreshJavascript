586b8e1040ff136478158b9845913475d6faadd7
Şaban Kayış <sbnkys1@gmail.com>
2021-04-28 08:36:05 +0300

    core syntax javascript

---
+++ play.js
@@ -0,0 +1,15 @@
+var namee = "Max";
+var age = 29;
+var hasHobbies = true;
+function summarizeUser( userName, userAge, userHasHobby){
+    return(
+        "Name is"+
+        userName + 
+        ", age is" +
+        userAge +
+        "and the user has hobbies" +
+        userHasHobby
+    );
+};
+
+console.log(summarizeUser(namee,age,hasHobbies));
